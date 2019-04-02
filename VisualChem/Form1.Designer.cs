9aa2febc04eaec2831d81727b22bddda6ba2745e
WeekendGamer8900 <henry.5a1888@gmail.com>
2019-04-02 18:07:46 +0800

    Enlarged app

--- VisualChem/Form1.Designer.cs
+++ VisualChem/Form1.Designer.cs
@@ -42,9 +42,9 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.imgOut.BackColor = System.Drawing.Color.White;
             this.imgOut.Location = new System.Drawing.Point(0, 0);
-            this.imgOut.Margin = new System.Windows.Forms.Padding(4);
+            this.imgOut.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.imgOut.Name = "imgOut";
-            this.imgOut.Size = new System.Drawing.Size(814, 450);
+            this.imgOut.Size = new System.Drawing.Size(1221, 703);
             this.imgOut.TabIndex = 0;
             this.imgOut.TabStop = false;
             this.imgOut.MouseDown += new System.Windows.Forms.MouseEventHandler(this.imgOut_MouseDown);
@@ -55,10 +55,10 @@
             // 
             this.txtName.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.txtName.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtName.Location = new System.Drawing.Point(0, 453);
-            this.txtName.Margin = new System.Windows.Forms.Padding(4);
+            this.txtName.Location = new System.Drawing.Point(0, 721);
+            this.txtName.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(814, 23);
+            this.txtName.Size = new System.Drawing.Size(1221, 23);
             this.txtName.TabIndex = 1;
             this.txtName.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             this.txtName.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtName_KeyUp);
@@ -71,14 +71,14 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(814, 476);
+            this.ClientSize = new System.Drawing.Size(1221, 744);
             this.Controls.Add(this.txtName);
             this.Controls.Add(this.imgOut);
-            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.KeyPreview = true;
-            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "Form1";
             this.Text = "VisualChem";
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
